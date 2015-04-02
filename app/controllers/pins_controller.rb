c3c12aea0a8072a58e2e0a1b8bd163aee8c23806
kennytrionfo <kentrionfo@gmail.com>
2015-04-02 04:30:50 -0600

    created new and create actions in pins controller

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,4 +4,18 @@ class PinsController < ApplicationController
 		
 	end
 
+	def new
+		@pin = Pin.new
+	end
+
+	def create
+		@pin = Pin.new(pin_params)
+	end
+
+
+	private
+
+	def pin_params
+		params.require(:pin).permit(:title, :description)
+	end
 end
