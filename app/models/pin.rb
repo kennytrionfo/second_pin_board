c47d220ea18caf7c21987c5a7e13339ac639b6a0
kennytrionfo <kentrionfo@gmail.com>
2015-04-02 20:34:20 -0600

    made associations for pins and users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,2 +1,4 @@
 class Pin < ActiveRecord::Base
+	belongs_to :user
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150403022831) do
+ActiveRecord::Schema.define(version: 20150403023400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,10 @@ ActiveRecord::Schema.define(version: 20150403022831) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "pins", ["user_id"], name: "index_pins_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
