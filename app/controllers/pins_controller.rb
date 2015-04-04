459d4044846e2389124bbc6e665ba669efcfdf24
kennytrionfo <kentrionfo@gmail.com>
2015-04-04 04:14:41 -0600

    added pins button in nav bar

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,6 @@
 class PinsController < ApplicationController
 	before_action :find_pin, only: [:show, :edit, :update, :destroy]
-	before_action :authenticate_user!, except: [:index, :show]
+	before_action :authenticate_user!, except: [:welcome]
 
 	def welcome
 		
